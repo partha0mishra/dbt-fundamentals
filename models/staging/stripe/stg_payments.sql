d462a2b0e2eb983e352a6014231a6d88b9cbec80
partha.mishra@gmail.com <partha.mishra@gmail.com>
2021-12-16 17:38:14 +0000

    Jinja templating

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,5 +1,9 @@
-select orderid as order_id
+select 
+id as payment_id
+,orderid as order_id
+, paymentmethod as payment_method
 -- amount is stored in cents. converting to dollars.
 , amount/100 as amount
 , status
+, created as created_at
 from {{source('stripe','payment')}}
